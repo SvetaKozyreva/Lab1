6c4358487aa0cb22dbde05506603ed0e40f243ba
SvetaKozyreva <svtkozyreva@gmail.com>
2024-09-04 23:24:25 +0300

    1

--- Lab1_Example/Program.cs
+++ Lab1_Example/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string s = "Dfg+t5-r7y;asd*s34;rt";
+            int t = s.IndexOf(";");
+            int i, k = 0;
+            for (i = 0; i <= t; i++)
+            {
+                if (char.IsLetter(s[i])) k++;
+            }
+            Console.WriteLine("k = ", k);
         }
     }
 }
